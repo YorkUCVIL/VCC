aed0a6b597b3b4d4b07493685452d4cbf88485c4
mkowal2 <matt2kowal@gmail.com>
2024-06-11 15:25:51 -0400

    Up to class node

--- main.js
+++ main.js
@@ -1,34 +1,94 @@
-
-
 var draw = null; // this is your svg element
-var concept_size = 100;
+var concept_size = 11;
 var concept_spacing = 10;
 var layer_spacing = 20;
-var connectome_spec = {
-	size_spec: [2,4,4,1], // check len for n layers
-};
 
-function main(){
-	draw = SVG('diagram').size(600,600);
+// Load your JSON data
+var connectome_spec = null; // Will be loaded from the JSON file
 
-	// this is how you draw rects
-	//draw.rect().attr({width:concept_size, height:concept_size, fill:'red', x:0, y:0});
-
-	// draw tree
-	let n_layers = connectome_spec.size_spec.length;
-	for (let l=0;l<n_layers;l++){
-		let n_concepts = connectome_spec.size_spec[l];
-		for (let c=0;c<n_concepts;c++){
-			x_pos = l*(concept_size+layer_spacing); // we push box right as a multiple of the layer num and layer spacing
-			y_pos = c*(concept_size+concept_spacing);
-			draw.rect().attr({width:concept_size, height:concept_size, fill:'red', x:x_pos, y:y_pos});
-		}
-	}
-	console.log(n_layers);
+async function loadJSON() {
+    const response = await fetch('demo_outputs_processed/clip_r50_4Lay/apron/vcc_info.json');
+    connectome_spec = await response.json();
 }
 
+function drawNode(layer, concept, imageSrc, offsetY) {
+    let x_pos = layer * (concept_size + layer_spacing);
+    let y_pos = offsetY + concept * (concept_size + concept_spacing);
+    draw.image(imageSrc, concept_size, concept_size).move(x_pos, y_pos);
+    return {
+        x_left: x_pos,
+        y_middle: y_pos + concept_size / 2,
+        x_right: x_pos + concept_size,
+        y_middle: y_pos + concept_size / 2
+    };
+}
 
-// runs main function on document load
+function drawEdge(startPos, endPos, weight) {
+    console.log(`Drawing edge from ${startPos.x_right},${startPos.y_middle} to ${endPos.x_left},${endPos.y_middle} with weight ${weight}`);
+    draw.line(startPos.x_right, startPos.y_middle, endPos.x_left, endPos.y_middle).stroke({ width: weight, color: '#000', opacity: weight });
+}
+
+async function main() {
+    await loadJSON();
+    draw = SVG('diagram').size(800, 800);
+
+    let n_layers = connectome_spec.size_spec.length;
+    let positions = {};
+
+    // Calculate the maximum height of the canvas
+    let maxHeight = Math.max(...connectome_spec.size_spec.map(size => size * (concept_size + concept_spacing)));
+
+    // Draw nodes and store their positions
+    for (let l = 0; l < n_layers; l++) {
+        let layerName = connectome_spec.layers[l];
+        let n_concepts = connectome_spec.size_spec[l];
+        if (typeof n_concepts === 'string') n_concepts = parseInt(n_concepts);
+        positions[layerName] = [];
+
+        // Calculate the offset for centering the nodes in this layer
+        let layerHeight = n_concepts * (concept_size + concept_spacing);
+        let offsetY = (maxHeight - layerHeight) / 2;
+
+        for (let c = 0; c < n_concepts; c++) {
+            let imageSrc = connectome_spec.images[layerName][c] + ".png"; // Assuming image paths are saved without the .png extension
+            let pos = drawNode(l, c, imageSrc, offsetY);
+            positions[layerName].push(pos);
+        }
+    }
+
+    // Draw edges
+    for (let layerName in connectome_spec.edge_weights) {
+        let edges = connectome_spec.edge_weights[layerName];
+        for (let edge in edges) {
+            console.log((edge));
+            let [deeperLayer, earlyLayer] = edge.split("-");
+            let [deeperConceptIndex, earlyConceptIndex] = [parseInt(deeperLayer.split(" ")[1].replace("apron_concept", "")) - 1, parseInt(earlyLayer.split(" ")[1].replace("apron_concept", "")) - 1];
+            if (deeperLayer.split(" ")[0] === "class"){
+                deeperConceptIndex = 1;
+            }
+            let deeperLayerName = deeperLayer.split(" ")[0];
+            let earlyLayerName = earlyLayer.split(" ")[0];
+
+            let weightList = edges[edge];
+            weightList.forEach(weight => {
+                let startPos = positions[earlyLayerName][earlyConceptIndex];
+                let endPos = positions[deeperLayerName][deeperConceptIndex];
+
+                if (startPos && endPos) {
+                    // check if class is present
+                    // if (deeperLayerName === "class") {
+                    //     drawEdge(startPos, endPos, weight);
+                    // }
+                    drawEdge(startPos, endPos, weight);
+                }
+            });
+        }
+    }
+
+    console.log(n_layers);
+}
+
+// Runs main function on document load
 document.addEventListener("DOMContentLoaded", function(event) {
-	main();
+    main();
 });
---
+++ process_vcc_dirs.py
@@ -0,0 +1,124 @@
+import os
+import pickle
+import math
+import json
+from tqdm import tqdm
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+# dir structure: parent_vcc_dir -> model_dir -> class_dir -> cd.pkl
+parent_vcc_dir = 'demo_outputs'
+save_dir = 'demo_outputs_processed'
+
+print('Saving processed data to:', save_dir)
+
+# make save_dir if it doesn't exist
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+
+# iterate over all model directories
+for model_dir in tqdm(os.listdir(parent_vcc_dir)):
+    model_demo_dir = os.path.join(parent_vcc_dir, model_dir)
+    if not os.path.isdir(model_demo_dir):
+        continue
+
+    print('Processing model:', model_dir)
+    # iterate over all class directories
+    for class_dir in os.listdir(model_demo_dir):
+        class_demo_dir = os.path.join(model_demo_dir, class_dir)
+        if not os.path.isdir(class_demo_dir):
+            continue
+
+        # iterate over all pickle files
+        pickle_file = 'cd.pkl'
+
+        pickle_file = os.path.join(class_demo_dir, pickle_file)
+        if os.path.exists(pickle_file):
+            with open(pickle_file, 'rb') as f:
+                    cd = pickle.load(f)
+
+            vcc_dic = {}
+            vcc_dic['edge_weights'] = cd.edge_weights
+            # average the edge weights
+            for k, v in vcc_dic['edge_weights'].items():
+                vcc_dic['edge_weights'][k] = {k2: np.mean(v2) for k2, v2 in v.items()}
+            vcc_dic['images'] = {}
+            vcc_dic['size_spec'] = [len(cd.dic[bn]['concepts']) for bn in cd.bottlenecks] + ['1']
+            vcc_dic['layers'] = cd.bottlenecks + ['class']
+
+            # create new directory for processed data
+            processed_dir = os.path.join(save_dir, model_dir, class_dir)
+            if not os.path.exists(processed_dir):
+                os.makedirs(processed_dir)
+
+            print('processing class:', class_dir)
+
+            for concept_layer in cd.bottlenecks + ['class']:
+                processed_layer_dir = os.path.join(processed_dir, concept_layer)
+                if not os.path.exists(processed_layer_dir):
+                    os.makedirs(processed_layer_dir)
+
+                if concept_layer.split(' ')[0] == 'class':
+                    vcc_dic['images'][concept_layer] = [os.path.join(processed_dir, concept_layer, 'class')]
+                    concept_name = 'class'
+                    canvas = np.ones((224 * 3,
+                                           224 * 3, 3))
+                    for i in range(int(3 ** 2)):
+                        row = math.floor(i / 3)
+                        col = i % 3
+                        canvas[(row * 224):(row + 1) * 224,
+                        col * 224:(col + 1) * 224, :] = \
+                        cd.discovery_images[i]
+                else:
+                    vcc_dic['images'][concept_layer] = [os.path.join(processed_dir, concept_layer, concept) for concept
+                                                        in cd.dic[concept_layer]['concepts']]
+                    for concept_name in cd.dic[concept_layer]['concepts']:
+
+                        # get img for low_node
+                        if isinstance(cd.dic[concept_layer][concept_name]['images'], list):
+                            low_concept_images = np.array(
+                                [np.load(x) for x in cd.dic[concept_layer][concept_name]['images']])
+                            low_concept_patches = np.array(
+                                [np.load(x) for x in cd.dic[concept_layer][concept_name]['patches']])
+                        else:
+                            low_concept_images = cd.dic[concept_layer][concept_name]['images']
+                            low_concept_patches = cd.dic[concept_layer][concept_name]['patches']
+
+                        low_concept_img_numbers = cd.dic[concept_layer][concept_name]['image_numbers']
+                        idxs = np.arange(low_concept_patches.shape[0])[:int(3 ** 2)]
+
+                        canvas = np.ones((low_concept_images.shape[2] * 3,
+                                              low_concept_images.shape[1] * 3, 3))
+                        for i, idx in enumerate(idxs):
+                            row = math.floor(i / 3)
+                            col = i % 3
+
+                            img = cd.discovery_images[low_concept_img_numbers[idx]]
+                            mask = 1 - (np.mean(low_concept_patches[idx] == float(
+                                cd.average_image_value) / 255, -1) == 1)
+                            mask_expanded = np.expand_dims(mask, -1)
+                            img_viz_blend = ((mask_expanded * img) + ((1 - mask_expanded) * img * 0.3))
+                            ones = np.where(mask == 1)
+                            h1, h2, w1, w2 = ones[0].min(), ones[0].max(), ones[1].min(), ones[1].max()
+                            img_viz = Image.fromarray((img_viz_blend[h1:h2, w1:w2] * 255).astype(np.uint8))
+                            img_viz = np.array(img_viz.resize((224, 224), Image.Resampling.BICUBIC)).astype(
+                                float) / 255
+
+                            # plot patch
+                            canvas[(row * low_concept_images.shape[2]):(row + 1) * low_concept_images.shape[2],
+                            col * low_concept_images.shape[1]:(col + 1) * low_concept_images.shape[1], :] = img_viz
+
+                        # show image with matplotlib
+                        # plt.imshow(canvas)
+                        # plt.show()
+
+                # save image
+                canvas = Image.fromarray((canvas * 255).astype(np.uint8))
+                canvas.save(os.path.join(processed_layer_dir, f'{concept_name}.png'))
+
+
+            # save json file
+            vcc_dic_save_path = os.path.join(processed_dir, 'vcc_info.json')
+            with open(vcc_dic_save_path, 'w') as f:
+                json.dump(vcc_dic, f)
+print('Done!')
